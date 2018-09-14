b5c478247fd52e7a1824c4552decafd7cecd1912
Carlos Eduardo Justino <carlos.justino08@gmail.com>
2018-09-14 20:49:28 -0300

    Avaliação de segurança - ajuste de layout

--- src/main/resources/static/js/gumga-password-meter.js
+++ src/main/resources/static/js/gumga-password-meter.js
@@ -1,13 +1,29 @@
-function verify(snh) {
+function chkPass(snh) {
     $.ajax({
         type: 'POST',
         url: '/verify',
         data: JSON.stringify({snh: snh}),
         success: function (data) {
+            var complexity = $('#complexity');
             $('#score').text(data.score);
-            $('#complexity').text(data.complexity);
+            complexity.text(data.complexity);
+            complexity.removeClass();
+            complexity.addClass("badge");
+            if (parseInt(data.score) >= 80){
+                complexity.addClass("badge-success");
+            }else if (parseInt(data.score) >= 60){
+                complexity.addClass("badge-primary");
+            }else if (parseInt(data.score) >= 40){
+                complexity.addClass("badge-info");
+            }else if (parseInt(data.score) >= 20){
+                complexity.addClass("badge-warning");
+            }else {
+                complexity.addClass("badge-danger");
+            }
         },
         contentType: "application/json",
         dataType: 'json'
     });
+
 }
+
